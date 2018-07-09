fb0e4575acd00c4028d0f9e12360a72848d7ec04
ivarprudnikov <ivar.prudnikov@gmail.com>
2018-07-09 18:32:15 +0100

    expose all files in bucket

--- app/js/config.js
+++ app/js/config.js
@@ -1,5 +1,5 @@
  var S3BL_IGNORE_PATH = true;
  var BUCKET_NAME = 'storage.dasmicrobot.com';
  var BUCKET_URL = 'http://' + BUCKET_NAME + '.s3.amazonaws.com';
- var S3B_ROOT_DIR = 'raspberry/';
+ var S3B_ROOT_DIR = '';
  var S3B_SORT = 'DEFAULT';
